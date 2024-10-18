52965bebb4cffd136aab9f11c0b53c7c8dbd5e0f
batulile96 <batulilentsethe@gmail.com>
2024-10-18 09:12:55 +0200

    i have created my portfolio

--- weather.js
+++ weather.js
@@ -72,3 +72,19 @@ getWeather();
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('menu-active');
 });
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+AIzaSyAbqj-IIN3jLBT_J9pZOQcIKbxJhacgtJc
